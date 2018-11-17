5a6b6bbed52ae4d2f4bf4cdd65bd535d3ca40131
Sakdipat <sakdipatvijit@hotmail.com>
2018-11-17 17:20:22 +0000

    page #

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,9 +2,13 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        @todo_description = "Polm estimate"
-        @todo_pomodoro_estimate = 4
-
+       todo_id = params[:id]
+        if todo_id=="1" # [Clean room]
+         @todo_description = "Polm estimate"
+            @todo_pomodoro_estimate = 4
+            elsif todo_id=="2"
+           @todo_description = "Polm est"
+            @todo_pomodoro_estimate = 4 
+        end
     end
 end
-
